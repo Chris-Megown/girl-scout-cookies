1e9c642ec38f0c93c684c42923f57819fcbc54fb
Christopher A Megown <megown1204@gmail.com>
2017-09-14 12:53:26 -0400

    hazaaaaa

--- src/CookieOrder.java
+++ src/CookieOrder.java
@@ -3,11 +3,14 @@ public class CookieOrder {
 
 	private String variety;
 	private int numBoxes;
-	
+
 	public CookieOrder(String variety, int numBoxes) {
 		this.variety = variety;
 		this.numBoxes = numBoxes;
-		
 	}
-	
+	public int getNumBoxes() {
+		return numBoxes;
+	}
+
+
 }
--- src/MasterOrder.java
+++ src/MasterOrder.java
@@ -1,7 +1,27 @@
 
 import java.util.ArrayList;
 
+import shopping.Item;
+
 public class MasterOrder {
 
 	ArrayList<CookieOrder> order = new ArrayList<>();
+
+	// public MasterOrder() {
+
+	public void addOrder(CookieOrder theOrder) {
+		order.addCookieOrder("Tagalongs", 2);
+	}
+
+	public int getTotalBoxexs() {
+		int totalBoxes = 0;
+		for (CookieOrder theOrder : order) {
+			totalBoxes += theOrder.getNumBoxes();
+		}
+		return totalBoxes;
+	}
+
+	public ArrayList<CookieOrder> getItems() {
+		return order;
+	}
 }
